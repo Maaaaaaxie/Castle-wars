e375c83e44aece68307e166a71407830faa7b736
Martin Rempel <martin.rempel@gicom.org>
2018-06-28 12:41:50 +0200

    *formatted index.js (tabs instead of spaces), added support to send mock data, +added mock.html and base.css for design testing

--- index.js
+++ index.js
@@ -7,22 +7,36 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
+const fnServeFiles = (req, res) => {
+	switch (req.url) {
+		case "/mock.html":
+		case "/base.css":
+			res.sendFile(path.join(__dirname, req.url.slice(1)));
+			break;
+		default:
+			res.sendFile(INDEX);
+			break;
+	}
+};
+
 // Start server
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
- .listen(PORT, () => console.log("Listening on localhost:" + PORT));
+const
+server = express()
+	// .use((req, res) => res.sendFile(INDEX))
+	.use(fnServeFiles)
+	.listen(PORT, () => console.log("Listening on localhost:" + PORT));
 
 // Initiatlize SocketIO
 const io = socketIO(server);
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function (socket) {
+	console.log('a user connected');
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+	});
 
-  socket.on('chat', function(msg){
-    console.log('message: ' + msg);
-  });
+	socket.on('chat', function (msg) {
+		console.log('message: ' + msg);
+	});
 });
