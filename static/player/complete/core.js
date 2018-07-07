77cac1f1d5f80033cc8b54ac53e5baf924e247af
Martin Rempel <martin.rempel@gicom.org>
2018-07-07 23:18:49 +0200

    *refactored render.js to core.js

---
+++ static/player/complete/core.js
@@ -0,0 +1,7 @@
+requirejs(["../information/information"/*, "./cards", "./stats"*/], (Title, Cards, Stats) => {
+	"use strict";
+
+	console.log("Rendering player view");
+
+	document.body.appendChild(Title);
+});
--- static/player/complete/render.js
+++
@@ -1,7 +0,0 @@
-requirejs(["./title"/*, "./cards", "./stats"*/], (Title, Cards, Stats) => {
-	"use strict";
-
-	console.log("Rendering player view");
-
-	document.body.appendChild(Title);
-});
