49afa175bcd66f36558fecf9f5547106779a54d4
Martin Rempel <martin.rempel@gicom.org>
2018-07-11 11:02:02 +0200

    + added resources mock files * changed resources mock + added ajax class (will be removed later I guess)

---
+++ static/player/ajax.js
@@ -0,0 +1,23 @@
+define(() => {
+	"use strict";
+
+	/**
+	 * @define class AJAX
+	 */
+	return class AJAX {
+		constructor(sUrl, sMethod = "GET", bJSON = true) {
+			return new Promise((fnResolve, fnReject) => {
+				this._xhr = new XMLHttpRequest();
+				this._xhr.open(sMethod, sUrl);
+				this._xhr.onload = () => {
+					if (this._xhr.status === 200) {
+						fnResolve(bJSON ? JSON.parse(this._xhr.responseText) : this._xhr.responseText);
+					} else {
+						fnReject(this._xhr.status);
+					}
+				};
+				this._xhr.send();
+			});
+		}
+	}
+});
--- static/player/information/information.js
+++ static/player/information/information.js
@@ -1,36 +1,50 @@
-define(["../complete/cssLoader"], (CSSLoader) => {
+define(["../complete/cssLoader", "./timer"], (CSSLoader, Timer) => {
 	"use strict";
 
 	// import stylesheet necessary for this section
 	CSSLoader.loadStyleSheet("/player/information", "information");
 
-	const oSection = document.createElement("section");
-	oSection.setAttribute("id", "information");
+	/**
+	 * @define class Information
+	 */
+	return class Information extends Timer {
+		static render() {
+			/* oSection:
+				<section id="information">
+					<article id="infotext">
+						<span>Du bist dran!</span>
+					</article>
+					<article id="timer">30</article>
+				</section>
+			*/
 
-	const oInfoText = document.createElement("article");
-	oInfoText.setAttribute("id", "infotext");
-	oSection.appendChild(oInfoText);
+			const oSection = document.createElement("section");
+			oSection.setAttribute("id", "information");
 
-	// TODO: oInfo needs an ID so the text can be updated
-	const oInfo = document.createElement("span");
-	oInfo.innerText = "Du bist dran!";
-	oInfoText.appendChild(oInfo);
+			const oInfoText = document.createElement("article");
+			oInfoText.setAttribute("id", "infotext");
+			oSection.appendChild(oInfoText);
 
-	const oTimer = document.createElement("article");
-	oTimer.setAttribute("id", "timer");
-	oTimer.innerText = 30;
-	oSection.appendChild(oTimer);
+			// TODO: oInfo needs an ID so the text can be updated
+			const oInfo = document.createElement("span");
+			oInfo.setAttribute("id", "currentPlayer");
+			oInfo.innerText = "Du bist dran!";
+			oInfoText.appendChild(oInfo);
 
-	// TODO: create/add Timer function
+			const oTimer = document.createElement("article");
+			oTimer.setAttribute("id", "timer");
+			oTimer.innerText = 30;
+			oSection.appendChild(oTimer);
 
-	return oSection;
-
-	/* oSection:
-		<section id="information">
-			<article id="infotext">
-				<span>Du bist dran!</span>
-			</article>
-			<article id="timer">30</article>
-		</section>
-	*/
+			return oSection;
+		}
+		static setTurn(bCurrentPlayer = true) {
+			const oActivePlayer = document.getElementById("currentPlayer");
+			if (bCurrentPlayer) {
+				oActivePlayer.innerText = "Du bist dran!";
+			} else {
+				oActivePlayer.innerText = "Dein Gegner ist dran!";
+			}
+		}
+	};
 });
